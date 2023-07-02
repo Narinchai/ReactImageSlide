39c1f7a306d79ee37ef47a878775109aec43afdb
Narinchai <narinpetes@gmail.com>
2023-07-02 23:04:33 +0700

    Image Slide

---
+++ image-slider/src/App.js
@@ -0,0 +1,13 @@
+import ImageSlider from "./components/ImageSlider";
+import './App.css'
+import { AiOutlineArrowLeft,AiOutlineArrowRight } from "react-icons/ai";
+
+function App() {
+  return (
+    <div className="App">
+     <ImageSlider/>
+    </div>
+  );
+}
+
+export default App;
---
+++ image-slider/src/components/ImageData.js
@@ -0,0 +1,18 @@
+const ImageData=[
+    {
+        title:"Sea",
+        image:"https://cdn.pixabay.com/photo/2023/06/15/10/03/surfing-8065035_1280.jpg"
+
+    },
+    {
+        title:"walrus",
+        image:"https://cdn.pixabay.com/photo/2023/06/15/11/04/huddle-of-walrus-8065100_1280.jpg"
+
+    },
+    {
+        title:"Boat",
+        image:"https://cdn.pixabay.com/photo/2023/05/18/16/25/boat-8002718_1280.jpg"
+
+    }
+]
+export default ImageData
